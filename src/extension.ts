2894a3a55ef356fea5641794fa0b8547e0c767d5
Thom van den Broek <thombroek1606@hotmail.com>
2020-07-21 18:35:22 +0200

    version 1.0.1 ready for release

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,7 @@
 import { ExtensionContext, window, StatusBarAlignment, StatusBarItem, workspace, commands } from 'vscode'
 import * as child_process from 'child_process'
+import * as fs from 'fs'
+import * as path from 'path'
 
 var s: StatusBarItem
 
@@ -7,17 +9,23 @@ export function activate(context: ExtensionContext) {
 	if(s) s.dispose()
 
 	const openFolder = commands.registerCommand('quickterminal.openfolder', e => {
+		// get default terminals from vscode
 		const cmd = workspace.getConfiguration('terminal').external
+		let p = e.fsPath
+		// get folder if the path is a file
+		if (!fs.lstatSync(p).isDirectory()) {
+			p = path.dirname(p)
+		}
+		// open terminal based on OS
 		if (process.platform === 'win32') {
-			child_process.exec(`start /D ${e.fsPath} "%cd%" cmd`)
+			child_process.exec(`start /D ${p} "%cd%" cmd`)
 		} else if (process.platform === 'darwin') {
-			child_process.exec(`open -a ${cmd.osxExec || 'Terminal.app'} "${e.fsPath}"`)
+			child_process.exec(`open -a ${cmd.osxExec || 'Terminal.app'} "${p}"`)
 		} else if (process.platform === 'linux') {
 			if (cmd.linuxExec) {
 				return window.showErrorMessage('You have not set your linux terminal. Please add the "terminal.external.linuxExec" to your settings.json')
 			}
-			
-			child_process.exec(`cd ${e.fsPath} && ${cmd.linuxExec}`)
+			child_process.exec(`cd ${e.fsPath} && ${p}`)
 		}
 	})
 	context.subscriptions.push(openFolder)
