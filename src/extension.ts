dcbae55365d0b5ff54da806429a05fe01ad570fa
Thom van den Broek <thombroek1606@hotmail.com>
2020-07-21 18:14:25 +0200

    support for opening folder

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,26 @@
-import {ExtensionContext, window, StatusBarAlignment, StatusBarItem, workspace} from 'vscode'
+import { ExtensionContext, window, StatusBarAlignment, StatusBarItem, workspace, commands } from 'vscode'
+import * as child_process from 'child_process'
 
 var s: StatusBarItem
 
 export function activate(context: ExtensionContext) {
 	if(s) s.dispose()
+
+	const openFolder = commands.registerCommand('quickterminal.openfolder', e => {
+		const cmd = workspace.getConfiguration('terminal').external
+		if (process.platform === 'win32') {
+			child_process.exec(`start /D ${e.fsPath} "%cd%" cmd`)
+		} else if (process.platform === 'darwin') {
+			child_process.exec(`open -a ${cmd.osxExec || 'Terminal.app'} "${e.fsPath}"`)
+		} else if (process.platform === 'linux') {
+			if (cmd.linuxExec) {
+				return window.showErrorMessage('You have not set your linux terminal. Please add the "terminal.external.linuxExec" to your settings.json')
+			}
+			
+			child_process.exec(`cd ${e.fsPath} && ${cmd.linuxExec}`)
+		}
+	})
+	context.subscriptions.push(openFolder)
 
 	workspace.onDidChangeConfiguration(() => {
 		if(s) {
